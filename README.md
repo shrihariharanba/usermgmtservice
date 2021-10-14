90daf73f7dfe0ad0a25423cfa66a3a411bdf701b
shrihariharan <hari@bluvision.com>
2021-10-14 23:16:53 +0530

    Read me instructions added

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# usermgmtservice
+#User Management
 User management Service
+
+Steps
+- Install mysql in your local machine
+- Configure the port and database url in the application.yml
+- Run the following command to start the application java -jar <jar file name>
--- src/main/java/org/usermgmt/App.java
+++ src/main/java/org/usermgmt/App.java
@@ -8,7 +8,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 /**
- * Hello world!
+ * User management Application
  */
 @SpringBootApplication(scanBasePackages = "org.usermgmt", exclude = {SecurityAutoConfiguration.class})
 public class App {
